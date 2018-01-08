713b2b375e7d926f9624ff29c5e6c5d1dcc69fcd
raj-andy1 <andyr@Andys-MBP.attlocal.net>
2018-01-07 21:28:40 -0800

    modified:   aws/araws1.tf

--- aws/araws1.tf
+++ aws/araws1.tf
@@ -15,12 +15,13 @@ resource "aws_instance" "exampleinstance" {
 security_groups = ["sg-15576a70","sg-934a74f6"]
 provisioner "remote-exec" {
     inline = [
-      "sudo curl -k https://ec2-52-38-176-168.us-west-2.compute.amazonaws.com:8140/packages/current/install.bash | bash -s extension_requests:pp_role=demo-webserver extension_requests:pp_created_by=AndyR extension_requests:pp_department=TSE",
+      "sudo curl -k https://ec2-52-38-176-168.us-west-2.compute.amazonaws.com:8140/packages/current/install.bash | sudo bash -s extension_requests:pp_role=demo-webserver extension_requests:pp_created_by=AndyR extension_requests:pp_department=TSE",
     ]
   connection {
     type = "ssh"
     user = "ec2-user"
     private_key = "${file("/Users/andyr/.ssh/id_rsa")}"
+    agent = true
   }
   }
 tags = {
